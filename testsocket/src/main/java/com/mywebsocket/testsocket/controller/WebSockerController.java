bf09e6ffe62ba1c1a0a723016fe9cd424db1e2ce
695372861 <langshixiong666@163.com>
2018-08-31 14:47:55 +0800

    增加websocket，添加异步上传文件条

--- testsocket/src/main/java/com/mywebsocket/testsocket/controller/WebSockerController.java
+++ testsocket/src/main/java/com/mywebsocket/testsocket/controller/WebSockerController.java
@@ -4,15 +4,24 @@ import com.mywebsocket.testsocket.entity.ChatRoomRequest;
 import com.mywebsocket.testsocket.entity.ChatRoomResponse;
 import com.mywebsocket.testsocket.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.List;
 
 @Controller
+@EnableScheduling//使用这个注解和@Scheduled这个注解配合进行定时执行一个方法
 public class WebSockerController {
 
     @Autowired
@@ -71,4 +80,60 @@ public class WebSockerController {
         this.template.convertAndSendToUser(chatRoomRequest.getUserId()+"","/alone/getResponse",response);
         return response;
     }
+
+    @RequestMapping("/upfile")
+    @ResponseBody
+    public String  upfile(HttpServletRequest req)
+    {
+        String userName=req.getParameter("userName");
+        System.out.println(userName);
+        String pathNameFile="";
+        List<MultipartFile> files = ((MultipartHttpServletRequest) req)
+                .getFiles("file");
+        MultipartFile file = null;
+        BufferedOutputStream stream = null;
+        String str=System.getProperty("user.dir");
+        str=str.replace("\\","/");
+        System.out.println(str);
+        str=str.substring(0,str.lastIndexOf("/"))+"/uploadfile/";
+        for (int i = 0; i < files.size(); i++) {
+            file = files.get(i);
+            if (!file.isEmpty()) {
+                try {
+                    String uploadFilePath = file.getOriginalFilename();
+                    System.out.println("uploadFlePath:" + uploadFilePath);
+                    // 截取上传文件的文件名
+                    String uploadFileName = uploadFilePath
+                            .substring(uploadFilePath.lastIndexOf('\\') + 1,
+                                    uploadFilePath.indexOf('.'));
+                    System.out.println("multiReq.getFile()" + uploadFileName);
+                    // 截取上传文件的后缀
+                    String uploadFileSuffix = uploadFilePath.substring(
+                            uploadFilePath.indexOf('.') + 1, uploadFilePath.length());
+                    System.out.println("uploadFileSuffix:" + uploadFileSuffix);
+                    //文件的路径名
+                    pathNameFile=str+"123" + uploadFileName + "." + uploadFileSuffix;
+                    stream = new BufferedOutputStream(new FileOutputStream(new File(pathNameFile)));
+                    byte[] bytes = file.getBytes();
+                    stream.write(bytes,0,bytes.length);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (stream != null) {
+                            stream.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                System.out.println("上传文件为空");
+            }
+        }
+        System.out.println("文件接受成功了:"+pathNameFile+":"+files.size());
+        return "成功辣";
+    }
+
+
 }
